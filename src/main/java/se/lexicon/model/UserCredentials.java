f1db724c5e43ba5aef7de5ce1e00782a4c9acfc5
Erik Svensson <eosvensson@gmail.com>
2021-10-19 10:23:28 +0200

    Added UserCredentials

---
+++ src/main/java/se/lexicon/model/UserCredentials.java
@@ -0,0 +1,75 @@
+package se.lexicon.model;
+
+import java.util.UUID;
+
+public class UserCredentials {
+
+    private final String id;
+    private String username;
+    private String password;
+    private String role;
+
+    /**
+     *
+     * @param id String <b>Not null</b>
+     * @param username String <b>Not null</b>
+     * @param password String <b>Not null</b>
+     * @param role String <b>Not null</b>
+     * @throws RuntimeException when anything is null
+     */
+    public UserCredentials(String id, String username, String password, String role) throws RuntimeException {
+        if(id == null){
+            throw new RuntimeException("id was null");
+        }
+        this.id = id;
+        setUsername(username);
+        setPassword(password);
+        setRole(role);
+    }
+
+    public UserCredentials(String username, String password, String role) {
+        this(UUID.randomUUID().toString(), username, password, role);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     *
+     * @param username String <b>Not null</b>
+     * @throws RuntimeException when username is null
+     */
+    public void setUsername(String username) throws RuntimeException {
+        if(username == null){
+            throw new RuntimeException("Username was null");
+        }
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if(password == null) {
+            throw new RuntimeException("Password was null");
+        }
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        if(role == null){
+            throw new RuntimeException("Role was null");
+        }
+        this.role = role;
+    }
+}
