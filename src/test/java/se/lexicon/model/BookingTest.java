a173bc05c12b93a5989f3e73f8488ff1e0796b66
Erik Svensson <eosvensson@gmail.com>
2021-10-20 11:44:52 +0200

    BookingTest added

---
+++ src/test/java/se/lexicon/model/BookingTest.java
@@ -0,0 +1,53 @@
+package se.lexicon.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class BookingTest {
+
+    public static final LocalDateTime DATE_TIME = LocalDateTime.parse("2022-01-07T13:00");
+    public static final double PRICE = 400;
+    public static final String VACCINE_ID = "Season flu";
+    private Booking testObject;
+
+
+    @Before
+    public void setUp() {
+        Premises premises = new Premises("Vårdcentral södra", new Address("Södergatan 3", "45263", "Byhåla"));
+        testObject = new Booking(
+                DATE_TIME,
+                PRICE,
+                VACCINE_ID,
+                premises
+        );
+    }
+
+    @Test
+    public void testObject_successfully_instantiated() {
+        assertNotNull(testObject);
+        assertNotNull(testObject.getId());
+        assertEquals(DATE_TIME, testObject.getDateTime());
+        assertNull(testObject.getAdministrator());
+        assertEquals(VACCINE_ID, testObject.getVaccineId());
+        assertEquals(PRICE, testObject.getPrice(), 0);
+        assertTrue(testObject.isVacant());
+        assertNotNull(testObject.getPremises());
+        assertNull(testObject.getPatient());
+    }
+
+    @Test
+    public void given_patient_setPatient_will_update_vacant_to_false() {
+        Patient patient = new Patient(
+                null, null, null, null, new UserCredentials("", "", ""), null
+        );
+
+        testObject.setPatient(patient);
+
+        assertNotNull(testObject.getPatient());
+        assertFalse(testObject.isVacant());
+    }
+}
